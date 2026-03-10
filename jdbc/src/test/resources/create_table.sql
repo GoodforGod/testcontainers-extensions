ca1aa712d8d37e9ef438a00e71dee152986300a7
Anton Kurako <goodforgod.dev@gmail.com>
2026-03-10 14:44:54 +0300

    Added FlywayJdbcMigrationEngine execute not in transaction to avoid complex request locking for Postgres and etc

---
+++ jdbc/src/test/resources/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id INT PRIMARY KEY,
+    email VARCHAR NULL,
+    username VARCHAR NULL,
+    status VARCHAR NULL
+);
--- jdbc/src/test/resources/db/migration/V1__flyway.sql
+++ jdbc/src/test/resources/db/migration/V1__flyway.sql
@@ -1,4 +1,10 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id INT NOT NULL PRIMARY KEY
+    id INT PRIMARY KEY,
+    email VARCHAR NULL,
+    username VARCHAR NULL,
+    status VARCHAR NULL
 );
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_users_email ON users (email);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_users_username ON users (username);
